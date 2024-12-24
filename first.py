09abbf0dd8410563176aed338fb11715783bf4b8
kiran <ksangale2109@gmail.com>
2024-12-24 16:44:00 +0530

    Resolve merge conflicts

--- first.py
+++ first.py
@@ -1,1 +1,17 @@
 print("hi")
+
+'''
+#new repo
+echo "# Python" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin git@github.com:Kiran242/Python.git
+git push -u origin main
+
+#existing repo
+git remote add origin git@github.com:Kiran242/Python.git
+git branch -M main
+git push -u origin main
+'''
